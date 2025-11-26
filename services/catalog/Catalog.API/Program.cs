538ff86cce150a3f6c38d51ff018918bc9215456
entsarahmed <entesara500@gmail.com>
2025-11-26 23:46:05 +0200

    Providing MetaData to Swagger and Change Port

--- services/catalog/Catalog.API/Program.cs
+++ services/catalog/Catalog.API/Program.cs
@@ -5,7 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version= "v1",
+        Description="This is API for Catalog Microservices in Ecommerce Application",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name =  "Entsar Ahmed",
+            Email="eentsaraahmed@gmail.com",
+            Url =new Uri("https://www.facebook.com/entesar.ahmed.902")
+        }
+    });
+});
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
