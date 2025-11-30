0be8a9cf7f9a0c70186833bf1591d6de1b3d137c
entsarahmed <entesara500@gmail.com>
2025-12-01 00:07:41 +0200

    Seed Data Part02

---
+++ services/catalog/Catalog.Infrastructure/Data/Contexts/ProductContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data.Contexts;
+public static class ProductContextSeed
+{
+   public static async Task DataSeedAsync(IMongoCollection<Product> productCollection)
+    {
+        var hasProduct = await productCollection.Find(_ => true).AnyAsync();
+        if (hasProduct)
+            return;
+        var filePath = Path.Combine("Data", "DataSeed", "products.json");
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException($"this filePath Not exist {filePath}");
+        var productJson = await File.ReadAllTextAsync(filePath);
+        var products = JsonSerializer.Deserialize<List<Product>>(productJson);
+        if (products?.Any() is true)
+            await productCollection.InsertManyAsync(products);
+
+    }
+}
