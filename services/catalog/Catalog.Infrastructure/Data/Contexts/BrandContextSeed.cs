4db8d3d76ad650b3506e1c521be8c2fdfdbcb1c8
entsarahmed <entesara500@gmail.com>
2025-11-30 22:39:33 +0200

    Seeding Data Part01

---
+++ services/catalog/Catalog.Infrastructure/Data/Contexts/BrandContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data.Contexts;
+public static class BrandContextSeed
+{
+    public static async Task SeedDataAsync(IMongoCollection<ProductBrand> brandCollection)
+    {
+        var hasBrands = await brandCollection.Find(_ => true).AnyAsync();
+       //Check on find Brands
+        if (hasBrands)
+            return;
+        //Get File Path as string
+        var filePath = Path.Combine("Data","SeedData","brands.json");
+         
+        //Check on file Path Do you Find?
+        if(!File.Exists(filePath))
+        {
+            Console.WriteLine($" Seed file Not Exists:{ filePath}");
+            return;
+        }
+        //Read Data from file
+        var brandData = await File.ReadAllTextAsync(filePath);
+        var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+        //Check on Data
+        if(brands?.Any() is true)
+        {
+            await brandCollection.InsertManyAsync(brands);
+        }
+    }
+}
