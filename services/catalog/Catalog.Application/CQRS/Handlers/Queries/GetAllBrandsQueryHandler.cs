698a00aed3d2d513f83c4c2f98cf7a4f7f2c4850
entsarahmed <entesara500@gmail.com>
2025-12-02 16:36:29 +0200

    Creating Brand Handler

---
+++ services/catalog/Catalog.Application/CQRS/Handlers/Queries/GetAllBrandsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Catalog.Application.CQRS.Queries;
+using Catalog.Application.ResponseDtos;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.CQRS.Handlers.Queries;
+public class GetAllBrandsQueryHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponseDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly IBrandRepository _brandRepository;
+
+    public GetAllBrandsQueryHandler(IMapper mapper, IBrandRepository brandRepository)
+    {
+        _mapper=mapper;
+        _brandRepository=brandRepository;
+    }
+    //What is Cancellation Token  ==> Object send with any progress Asynchronous  => if make cancel for Progress like user make request after it's close page ==> Cancellation Token Know stop progress treatment
+    public async Task<IList<BrandResponseDto>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        var BrandList = await _brandRepository.GetAllBrands();
+        var BrandResponseList = _mapper.Map<IList<ProductBrand>, IList<BrandResponseDto>>(BrandList.ToList());
+        return BrandResponseList;
+    }
+}
---
+++ services/catalog/Catalog.Application/Mappers/CatalogMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Catalog.Application.ResponseDtos;
+using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Mappers;
+public class CatalogMappingProfile: Profile
+{
+    public CatalogMappingProfile()
+    {
+        // ReverseMap ==> Convert from ProductBrand to BrandResponseDto or BrandResponseDto to ProductBrand
+        CreateMap<ProductBrand, BrandResponseDto>().ReverseMap();
+    }
+}
