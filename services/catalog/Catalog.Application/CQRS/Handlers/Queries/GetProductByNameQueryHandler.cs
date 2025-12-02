a0204e2e43ca64645df9b78f46fcac62a3950055
entsarahmed <entesara500@gmail.com>
2025-12-02 22:54:19 +0200

    Get Products By Name Handler

---
+++ services/catalog/Catalog.Application/CQRS/Handlers/Queries/GetProductByNameQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Catalog.Application.CQRS.Queries;
+using Catalog.Application.ResponseDtos;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.CQRS.Handlers.Queries;
+public class GetProductByNameQueryHandler : IRequestHandler<GetProductByNameQuery, IList<ProductResponseDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly IProductRepository _productRepository;
+
+    public GetProductByNameQueryHandler(IMapper mapper, IProductRepository productRepository)
+    {
+        _mapper=mapper;
+        _productRepository=productRepository;
+    }
+    
+   public async Task<IList<ProductResponseDto>> Handle(GetProductByNameQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetAllProductsByName(request.Name);
+        var productResponse = _mapper.Map<IList<ProductResponseDto>>(product);
+        return productResponse;
+    }
+}
