8d0cdee5db794fe1c4e2637f890a9a7aad55dc9e
entsarahmed <entesara500@gmail.com>
2025-12-03 16:57:48 +0200

    Creating Catalog Controller

---
+++ services/catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Application.CQRS.Queries;
+using Catalog.Application.ResponseDtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers;
+public class CatalogController : BaseApiController
+{
+    private readonly IMediator _mediator;
+
+    public CatalogController(IMediator mediator)
+    {
+        _mediator=mediator;
+    }
+    [HttpGet]
+    [Route("[action]/{id}",Name ="GetProductById")]
+    [ProducesResponseType(typeof(ProductResponseDto),(int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public async Task<ActionResult<ProductResponseDto>> GetProductById(string id)
+    {
+        var query = new GetProductByIdQuery(id);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
