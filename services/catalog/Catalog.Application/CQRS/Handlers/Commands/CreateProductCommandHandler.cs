89a81626d986e41e57a1c25a3a2f456221a88538
entsarahmed <entesara500@gmail.com>
2025-12-03 13:47:49 +0200

    Create Product Command

---
+++ services/catalog/Catalog.Application/CQRS/Handlers/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Catalog.Application.CQRS.Commands;
+using Catalog.Application.ResponseDtos;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.CQRS.Handlers.Commands;
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductResponseDto>
+{
+    private readonly IMapper _mapper;
+    private readonly IProductRepository _productRepository;
+
+    public CreateProductCommandHandler(IMapper mapper,IProductRepository productRepository )
+    {
+        _mapper=mapper;
+        _productRepository=productRepository;
+    }
+    public async Task<ProductResponseDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var productEntity = _mapper.Map<Product>(request);
+        var newProduct = await _productRepository.CreateProduct(productEntity);
+        var productResponse = _mapper.Map<ProductResponseDto>(newProduct);
+        return productResponse;
+
+    }
+}
