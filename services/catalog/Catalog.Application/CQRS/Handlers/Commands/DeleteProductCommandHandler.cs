e20b5a7dc5f4cdb73442debee64d934be777e70d
entsarahmed <entesara500@gmail.com>
2025-12-03 14:40:40 +0200

    Delete Product Command

---
+++ services/catalog/Catalog.Application/CQRS/Handlers/Commands/DeleteProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Application.CQRS.Commands;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.CQRS.Handlers.Commands;
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, bool>
+{
+    private readonly IProductRepository _productRepository;
+
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository=productRepository;
+    }
+    public async Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        return await _productRepository.DeleteProduct(request.Id);
+    }
+}
