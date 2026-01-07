dec73e9110b0bf954c1ce351e7d24b53eff6b3a4
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-07 16:49:29 +0200

    Define Catalog Specification

---
+++ services/catalog/Catalog.Core/Specs/CatalogSpecParams.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Specs
+{
+    public class CatalogSpecParams
+    {
+        private const int MaxPageSize = 80;
+        private int _pageSize = 10;
+        public int PageIndex { get; set; }
+        public int PageSize { 
+        get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+        public string? BrandId { get; set; }
+        public string? TypeId { get; set; }
+        public string? Sort { get; set; }
+        public string? Search { get; set; }
+    }
+}
