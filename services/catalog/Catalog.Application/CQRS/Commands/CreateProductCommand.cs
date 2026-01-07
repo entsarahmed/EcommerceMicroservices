fd9429a2581a1358133d1c8361581562bba4afa4
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-07 11:04:28 +0200

    nullable

--- services/catalog/Catalog.Application/CQRS/Commands/CreateProductCommand.cs
+++ services/catalog/Catalog.Application/CQRS/Commands/CreateProductCommand.cs
@@ -11,9 +11,9 @@ using System.Threading.Tasks;
 namespace Catalog.Application.CQRS.Commands;
 public class CreateProductCommand : IRequest<ProductResponseDto>
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Summary { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Summary { get; set; }
     public string ImageFile { get; set; }
     [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
     public decimal Price { get; set; }
