d865913822de761a16ab518637feb458221a2530
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-09 11:36:42 +0200

    solve Exception

--- services/catalog/Catalog.Core/Specs/CatalogSpecParams.cs
+++ services/catalog/Catalog.Core/Specs/CatalogSpecParams.cs
@@ -10,7 +10,7 @@ namespace Catalog.Core.Specs
     {
         private const int MaxPageSize = 80;
         private int _pageSize = 10;
-        public int PageIndex { get; set; }
+        public int PageIndex { get; set; } = 1;
         public int PageSize { 
         get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
