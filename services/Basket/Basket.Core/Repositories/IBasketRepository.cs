a3f49230c44d9135530d675682a29a7d226aed77
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-16 09:44:14 +0200

    Defining Basket Repository

---
+++ services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using Basket.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Core.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart cart);
+        Task DeleteBasket(string userName);
+    }
+}
