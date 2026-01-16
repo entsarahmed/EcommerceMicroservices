62f369c83c74485085aae70c832c0c7940098cac
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-16 13:12:21 +0200

    Implement Repository Part 1

--- services/Basket/Basket.Core/Repositories/IBasketRepository.cs
+++ services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -1,7 +1,4 @@
 ﻿using Basket.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Basket.Core.Repositories
 {
---
+++ services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await  _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null!;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket)!;
+        }
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
