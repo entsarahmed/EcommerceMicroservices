223f5c87ab867461654ca379d82031e02df0ac4c
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-16 08:41:15 +0200

    Create Entities Part 1

---
+++ services/Basket/Basket.Core/Entities/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Core.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ImageFile { get; set; }   
+
+    }
+}
