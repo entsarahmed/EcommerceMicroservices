7067b4505af686d78829387064b4224354586473
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-17 10:11:45 +0200

    Adding Response DTOs

---
+++ services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.Mappers
+{
+    public class BasketMappingProfile : Profile
+    {
+        public BasketMappingProfile()
+        {
+            
+        }
+    }
+}
