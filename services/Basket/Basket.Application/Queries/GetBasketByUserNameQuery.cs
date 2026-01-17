ffa70928df04c6b9a6449ead35106ebc00bec062
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-17 20:04:57 +0200

    Create GetBasketByUserName Query Handler

---
+++ services/Basket/Basket.Application/Queries/GetBasketByUserNameQuery.cs
@@ -0,0 +1,18 @@
+﻿using Basket.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.Queries
+{
+    public class GetBasketByUserNameQuery:IRequest<ShoppingCartResponse>
+    {
+        public string UserName { get; set; }
+        public GetBasketByUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+    }
+}
