a5b0b086594b80356471c5c99dd8348a454815df
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-17 23:18:26 +0200

    Create Shopping Cart Command Handler

---
+++ services/Basket/Basket.Application/Commands/CreateShoppingCartCommand.cs
@@ -0,0 +1,21 @@
+﻿using Basket.Application.Responses;
+using Basket.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.Commands
+{
+    public class CreateShoppingCartCommand:IRequest<ShoppingCartResponse>
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public CreateShoppingCartCommand(string userName, List<ShoppingCartItem> items)
+        {
+            UserName = userName;
+            Items = items;
+        }
+
+    }
+}
---
+++ services/Basket/Basket.Application/Handlers/Commands/CreateShoppingCartCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Basket.Application.Commands;
+using Basket.Application.Responses;
+using Basket.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.Handlers.Commands
+{
+    public class CreateShoppingCartCommandHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
+
+        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository, IMapper mapper)
+        {
+            _basketRepository = basketRepository;
+            _mapper = mapper;
+        }
+        public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            // ToDo will integrate with discout service
+            var shoppingCart = await _basketRepository.UpdateBasket(new Core.Entities.ShoppingCart
+            {
+                UserName = request.UserName,
+                Items = request.Items
+            });
+            var shoppingCartResponse = _mapper.Map<ShoppingCartResponse>(shoppingCart);
+            return shoppingCartResponse;
+        }
+    }
+}
