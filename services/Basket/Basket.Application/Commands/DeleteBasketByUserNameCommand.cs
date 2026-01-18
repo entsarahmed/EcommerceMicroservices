087d5729a8bddf75fadf3bccce3f1a0951490420
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-18 07:51:39 +0200

    Delete Basket By User Name Command Handler

---
+++ services/Basket/Basket.Application/Commands/DeleteBasketByUserNameCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.Commands
+{
+    public class DeleteBasketByUserNameCommand : IRequest<Unit>
+    {
+        public string UserName { get; set; }
+        public DeleteBasketByUserNameCommand(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
