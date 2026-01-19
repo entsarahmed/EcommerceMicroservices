2e67ecbc697a15ac6a301dd0cc3f2456466fce5e
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-19 16:05:14 +0200

    Create Basket Controller Part 2

--- services/Basket/Basket.Api/Controllers/BasketController.cs
+++ services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -1,8 +1,10 @@
-﻿using Basket.Application.Queries;
+﻿using Basket.Application.Commands;
+using Basket.Application.Queries;
 using Basket.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Basket.Api.Controllers
 {
@@ -17,11 +19,28 @@ namespace Basket.Api.Controllers
         }
         [HttpGet]
         [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        [ProducesResponseType(typeof(ShoppingCartItemResponse), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
         {
             var query = new GetBasketByUserNameQuery(userName);
             var basket = await _mediator.Send(query);
             return Ok(basket);
+        }
+
+        [HttpPost("CreateBasket")]
+        [ProducesResponseType(typeof(ShoppingCartItemResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateShoppingCartCommand command)
+        {
+            var basket = await _mediator.Send(command);
+            return Ok(basket);
+        }
+        [HttpDelete]
+        [Route("[action]/{userName}", Name = "DeleteBasketByUserName")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> DeleteBasket(string userName)
+        {
+            var command = new DeleteBasketByUserNameCommand(userName);
+            return Ok(await _mediator.Send(command));
         }
     }
 }
