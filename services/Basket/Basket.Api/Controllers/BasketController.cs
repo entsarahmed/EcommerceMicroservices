adb97b4c47915c2be8a4b95f69d339d9f75ade64
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-19 15:30:03 +0200

    Create Basket Controller

---
+++ services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,27 @@
+﻿using Basket.Application.Queries;
+using Basket.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+
+    public class BasketController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+
+        public BasketController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
+        {
+            var query = new GetBasketByUserNameQuery(userName);
+            var basket = await _mediator.Send(query);
+            return Ok(basket);
+        }
+    }
+}
