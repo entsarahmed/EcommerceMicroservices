cce2093bd1b2d3e5e56ff779200b0201a0117d16
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-31 00:15:51 +0200

    Deleted Discount Command & Deleted Discount Command Handler

---
+++ services/Discount/Discount.Application/Commands/DeleteDiscountCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Application.Commands
+{
+    public class DeleteDiscountCommand : IRequest<bool>
+    {
+        public string ProductName { get; set; }
+
+        public DeleteDiscountCommand(string productName)
+        {
+            ProductName = productName;
+        }
+    }
+}
---
+++ services/Discount/Discount.Application/Handlers/Commands/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Discount.Application.Commands;
+using Discount.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Application.Handlers.Commands
+{
+    public class DeleteDiscountCommandHandler : IRequestHandler<DeleteDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository;
+      
+
+        public DeleteDiscountCommandHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+            
+        }
+        public Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var deleted = _discountRepository.DeleteDiscount(request.ProductName);
+            return deleted;
+        }
+    }
+}
