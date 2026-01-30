30f2d97b66116f1ade88cdacbec2ac86cc1d8dd9
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-30 09:41:39 +0200

    Create Discount Profile

---
+++ services/Discount/Discount.Application/Mapper/DiscountProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Discount.Core.Entities;
+using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Application.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+
+        }
+    }
+}
