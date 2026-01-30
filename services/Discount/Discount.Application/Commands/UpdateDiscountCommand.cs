8b485ad70c8f1bdfdb4c8fe658114dcb2c750ffa
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-30 23:40:47 +0200

    Update Discount Command & Update Discount Command Handler

---
+++ services/Discount/Discount.Application/Commands/UpdateDiscountCommand.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Protos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Application.Commands
+{
+    public class UpdateDiscountCommand : IRequest<CouponModel>
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
