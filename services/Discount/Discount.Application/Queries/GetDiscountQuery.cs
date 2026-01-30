58c50d5e26654cc890b227e93b29a223a2c54741
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-30 09:48:30 +0200

    Create Get Discount Query

---
+++ services/Discount/Discount.Application/Queries/GetDiscountQuery.cs
@@ -0,0 +1,17 @@
+﻿using Discount.Grpc.Protos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Application.Queries
+{
+    public class GetDiscountQuery : IRequest<CouponModel>
+    {
+      public string ProductName { get; set; }
+        public GetDiscountQuery(string productName)
+        {
+            ProductName = productName;
+        }
+    }
+}
