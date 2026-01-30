c40d5caaf1059856680adbe67c211eaac8bea220
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-01-30 22:18:41 +0200

    Create Discount Command

---
+++ services/Discount/Discount.Application/Commands/CreateDiscountCommand.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Protos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Application.Commands
+{
+    public class CreateDiscountCommand : IRequest<CouponModel>
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+      
+    }
+}
