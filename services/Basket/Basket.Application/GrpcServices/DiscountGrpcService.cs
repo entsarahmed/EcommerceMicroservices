e3b68f2eb4ccc451d5ad89f0aeed33ab401ed229
entsarahmed <DELL@DESKTOP-SLMMA8F>
2026-02-20 04:41:27 +0200

    Create Discount GRPC Service

---
+++ services/Basket/Basket.Application/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountGrpcClient;
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountgrpcClient)
+        {
+            _discountGrpcClient = discountgrpcClient;  
+        }
+        public async Task<CouponModel> GetDiscount(string productName) {
+            var request = new GetDiscountRequest() {ProductName =productName };
+            return await _discountGrpcClient.GetDiscountAsync(request);
+        
+        }
+    }
+}
